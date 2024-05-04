d3cfda23124976f32200c8fede075d70d8e82d6c
root <mohammedahmed.refaat@gmail.com>
2024-05-04 02:09:27 -0700

    ./0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file named school in tmp dir
 
 file { '/tmp/school':
-  content  > 'I love Puppet',
-  mode     > '0744',
-  owner    > 'www-data',
-  group    > 'www-data',
+  content  => 'I love Puppet',
+  mode     => '0744',
+  owner    => 'www-data',
+  group    => 'www-data',
 }
