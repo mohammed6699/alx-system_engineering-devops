0369ede904610cf83803ecb79de7bbbe6e424d7d
root <mohammedahmed.refaat@gmail.com>
2024-06-03 14:14:26 -0700

    ./0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,7 +4,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = https://jsonplaceholder.typicode.com/
+    url = "https://jsonplaceholder.typicode.com/"
 
     employee_id = sys.argv[1]
 
