d16aafad672fcb40f44ef6c553da43e8c5957fa0
root <mohammedahmed.refaat@gmail.com>
2024-06-03 16:59:21 -0700

    ./all

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     USERNAME = user.get("USERNAME")
     params = {"userId": user_id}
     todos_response = requests.get(url + "todos", params=params)
-    todos = todos_response.json()
+    
     data_to_export = {user_id :[]}
 
     for todo in todos:
         task_info = {
-                "task": todo.get("title")
-                comleted: todo.get("completed")
+                "task": todo.get("title"),
+                "comleted": todo.get("completed"),
                 "USERNAME": USERNAME
                 }
         data_to_export[user_id].append(task_info)
